0da7f5df31e375c5a209907b39d68fdade3a3141
tabayashi <tabayashi@gmail.com>
2024-08-28 13:52:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,27 +18,27 @@ module.exports = postcss.plugin('postcss-custom-units', function (opts) {
 
     css.walkDecls(function (decl) {
       if (decl.prop.indexOf(prefix) === 0 && prefixRx.test(decl.prop)) {
-        /*
-        var unit = unitRx.exec(decl.value).slice(1, 3);
-        unit[0] = parseFloat(unit[0]) || null;
-
-        if (typeof unit[0] === 'number') {
-          units[decl.prop.slice(prefixLength)] = {value: unit[0], unit: unit[1]};
-          unitsRx = new RegExp('([\\d\\.]+)(' + Object.keys(units).join('|') + ')', 'gi');
-        }
-        */
-        units[decl.prop.slice(prefixLength)] = decl.value;
+        /**
+         * var unit = unitRx.exec(decl.value).slice(1, 3);
+         * unit[0] = parseFloat(unit[0]) || null;
+         * 
+         * if (typeof unit[0] === 'number') {
+         *   units[decl.prop.slice(prefixLength)] = {value: unit[0], unit: unit[1]};
+         *   unitsRx = new RegExp('([\\d\\.]+)(' + Object.keys(units).join('|') + ')', 'gi');
+         * }
+        **/
+        units[decl.prop.slice(prefixLength)] = decl.prop;
         unitsRx = new RegExp('([\\d\\.]+)(' + Object.keys(units).join('|') + ')', 'gi');
         return;
       }
 
       decl.value = decl.value.replace(unitsRx, function (m, value, unit) {
-        /*
-        value = (parseFloat(value) || 0) * units[unit].value;
-        unit = units[unit].unit;
-        return value + unit;
-        */
-        return `calc(${units[unit]} * ${value})`;
+        /**
+         * value = (parseFloat(value) || 0) * units[unit].value;
+         * unit = units[unit].unit;
+         * return value + unit;
+        **/
+        return `calc(var(${units[unit]}) * ${value})`;
       });
     });
   }
