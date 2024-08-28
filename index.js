e530fe00f8e9c759511d232b05d1996f93761956
tabayashi <tabayashi@gmail.com>
2024-08-28 13:34:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ module.exports = postcss.plugin('postcss-custom-units', function (opts) {
 
     css.walkDecls(function (decl) {
       if (decl.prop.indexOf(prefix) === 0 && prefixRx.test(decl.prop)) {
+        /*
         var unit = unitRx.exec(decl.value).slice(1, 3);
         unit[0] = parseFloat(unit[0]) || null;
 
@@ -25,14 +26,19 @@ module.exports = postcss.plugin('postcss-custom-units', function (opts) {
           units[decl.prop.slice(prefixLength)] = {value: unit[0], unit: unit[1]};
           unitsRx = new RegExp('([\\d\\.]+)(' + Object.keys(units).join('|') + ')', 'gi');
         }
-
+        */
+        units[decl.prop.slice(prefixLength)] = decl.value;
+        unitsRx = new RegExp('([\\d\\.]+)(' + Object.keys(units).join('|') + ')', 'gi');
         return;
       }
 
       decl.value = decl.value.replace(unitsRx, function (m, value, unit) {
+        /*
         value = (parseFloat(value) || 0) * units[unit].value;
         unit = units[unit].unit;
         return value + unit;
+        */
+        return `calc(${value} * ${units[unit]})`;
       });
     });
   }
